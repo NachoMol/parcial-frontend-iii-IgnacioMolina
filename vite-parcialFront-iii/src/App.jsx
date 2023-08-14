b8e833a4f0bedbe982d31b9827c855ef3e67ad65
NachoMol <ignaciotmolina@gmail.com>
2023-08-14 20:46:12 -0300

    agregando estilos

--- vite-parcialFront-iii/src/App.jsx
+++ vite-parcialFront-iii/src/App.jsx
@@ -1,9 +1,12 @@
 import Card from "./Components/Card.jsx";
+import './App.css'
 
 
 function App() {
   return (
     <>
+    <h1>Carga de estudiantes</h1>
+      <form></form>
       <Card />
       </>
   );
